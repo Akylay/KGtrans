08d3ae8b3fa003a199d55e755479a33d885f125b
KiraPubg <dragnilnatsu614@gmail.com>
2022-04-22 05:16:07 +0300

    scooter anim

--- js/main.js
+++ js/main.js
@@ -1,46 +1,21 @@
-const sections = document.querySelectorAll("section");
-const dots = document.querySelectorAll(".dot")
 
 
-sections.forEach(section => {
+function scrolTo(link, toLink, className, toClassName) {
 	document.addEventListener("scroll", function () {
-		const posTop = section.getBoundingClientRect().top;
-		const posBottom = section.getBoundingClientRect().bottom;
-		dots.forEach(el => {
-			const dot = el.getAttribute("href")
-		})
-		console.log("top: " + section.getAttribute("id") + posTop)
-		console.log("bottom: " + section.getAttribute("id") + posBottom)
-		section.classList.toggle("visible", posBottom <= 76);
-	});
-})
+		const thisClass = document.querySelector(`${className}`)
+		const posTop = thisClass.getBoundingClientRect().top
+		const posBottom = thisClass.getBoundingClientRect().bottom
+		const thisDot = document.querySelector(`[href="${link}"]`)
+		const toDot = document.querySelector(`[href="${toLink}"]`)
+		console.log("pb:" + posBottom, "pt:" + posTop )
+		if (posBottom <= 80) {
+			thisDot.classList.remove("dot-active")
+			toDot.classList.add("dot-active")
+		} else if (posBottom < 320) {
+			thisDot.classList.add("dot-active")
+			toDot.classList.remove("dot-active")
 
-// const header = document.querySelector("header"),
-// 	body = document.querySelector("body");
-// document.addEventListener("scroll", () => {
-// 	let scrollTop = body.scrollHeight;
-// 	console.log(scrollTop)
-// 	if (scrollTop >= 100) {
-// 		header.classList.add("hide")
-// 	} else if (scrollTop <= 100) {
-// 		header.classList.remove("hide")
-// 	}
-// })
-
-// const sections = document.querySelectorAll("section")
-
-// sections.forEach(section => {
-// 	const sectionID = section.getAttribute("id")
-// 	window.onscroll = function () {
-// 		let posTop = section.getBoundingClientRect().top
-// 		let posBottom = section.getBoundingClientRect().bottom
-// 		console.log(sectionID)
-// 		// if (posTop > 0) {
-// 		// 	section.classList.add("visible")
-// 		// }else if(posBottom = 0) {
-// 		// 	section.classList.remove("visible")
-// 		// }
-// 		// console.log(section.getAttribute("class") + " top " + posTop);
-// 		// console.log(section.getAttribute("class") + " bottom " + posBottom);
-// 	}
-// })
+		}
+	})
+}
+scrolTo("#hero", "#about", ".hero")
