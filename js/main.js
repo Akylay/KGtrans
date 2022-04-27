78fdfd1a59f5cfc9d4cdfe414e5a41fac459d8a1
Akylay <akylaymuseavaa@gmail.com>
2022-04-28 01:27:08 +0300

    added media

--- js/main.js
+++ js/main.js
@@ -37,3 +37,20 @@ function showSlides(n) {
 	sliderImage[slideIndex - 1].classList.add("active");
 	sliderInfo[slideIndex - 1].classList.add("active");
 }
+
+
+
+
+//
+// const openPopUp = document.getElementById("open_pop_up");
+// const closePopUp = document.getElementById("close_pop_up");
+// const popUp = document.getElementById("pop_up");
+//
+// openPopUp.addEventListener("click", function (e){
+// 	e.preventDefault();
+// 	popUp.classList.add("ask");
+// })
+//
+// closePopUp.addEventListener("click", () => {
+// 	popUp.classList.remove("ask");
+// })
